bb7f789eb02f194d5410f9e8f0b411e2b6a73c28
Guillaume Ebersohl <guigui1991@live.fr>
2025-04-07 10:35:18 +0200

    feat: first batch of parsing, need to check syntax

--- philo/srcs/init/parser.c
+++ philo/srcs/init/parser.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 
 #include "common.h"
+#include "philo.h"
 
 /**
  * Decision a prendre ici :
@@ -37,16 +38,57 @@ int	atou(const char *str)
 	return ((int)num);
 }
 
+int	literal_check(char **av)
+{
+	static int	i = 1;
+	int			j;
+
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j] && av[i][j] == ft_isdigit(av[i]))
+	}
+}
+
+int	check_params_value(char **av, t_data *data)
+{
+	if (data->nb_philos <= 0 || data->tt_die <= 0 || data->tt_eat <= 0 \
+		|| data->tt_sleep <= 0 || data->nb_meal < 0)
+	{
+		printf("error message, probably range error");
+		return (-1);
+	}
+	literal_check(av);
+	return (0);
+}
 
 /**
  * Next step: creer la structure principale qui stock les donnees, en commencant par le parsing des arguments.
  *
  */
-int	parser(int ac, char **av)
+int	parser(int ac, char **av, t_data * data)
 {
+	if (ac != 5 || ac != 6)
+	{
+		if (ac < 5)
+			printf(MISSING_ARGS);
+		else
+			printf(TOO_MANY_ARGS);
+		return (-1);
+	}
+	data->nb_philos = atou(av[1]);
+	data->tt_die = atou (av[2]);
+	data->tt_eat = atou(av[3]);
+	data->tt_sleep = atou(av[4]);
+	if (ac == 6)
+		data->nb_meal = atou(av[5]);
+	else
+		data->nb_meal = 0;
+	if (check_params_value(data) == -1)
+		return (-1);
 	if (/* si ac different de 4 ou 5 (5 ou 6),on degage*/)
 	//Si c'est pas le cas on convertis chaque valeur,
 	if (/*il y a un 6e arg on le convertis*/)
-	// check si un de ces valeurs est a -1, si c'est le cas message d'erreur de parsing general
+	// check si une de ces valeurs est a -1, si c'est le cas message d'erreur de parsing general
 	// en renvoie au main, si erreur il y a on renvoie -1 apres le message d'erreur.
 }
--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -1,9 +1,14 @@
+#include <string.h>
+
 #include "philo.h"
 
 int	main(int ac, char **av)
 {
-	(void) av;
-	parser(ac. av); // en sortie de ca on initialise la/les structures. (en ayant decide ce qu'on met dedans)
+	t_data	data;
+
+	memset(&data, 0, sizeof(t_data));
+	if (parser(ac. av, data) == -1)
+		return (-1); // en sortie de ca on initialise la/les structures. (en ayant decide ce qu'on met dedans)
 	// donc init data
 	// init thread/mutexes
 	// routine ? simulation ? pas compris encore ca
