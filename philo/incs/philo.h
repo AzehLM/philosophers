8a5843e2a65a5ccec64fb8eb0f0b018ab0b847a4
Guillaume Ebersohl <guigui1991@live.fr>
2025-04-07 10:34:41 +0200

    feat: main struct, only has parsing variables rn

--- philo/incs/philo.h
+++ philo/incs/philo.h
@@ -1,6 +1,16 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-void	util(int param);
+typedef struct s_data
+{
+	int			nb_meal;
+	int			nb_philos;
+	int			tt_die;
+	int			tt_eat;
+	int			tt_sleep;
+	long long	start;
+}	t_data;
+
+int	parser(int ac, char **av, t_data *data);
 
 #endif
